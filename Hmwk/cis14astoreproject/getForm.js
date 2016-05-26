df73e1bfbf58f48945ab1b94c2408079ebd39d29
Curt Burdette <clburdette@yahoo.com>
2016-05-25 23:18:32 -0700

    Signed-off-by: Curt Burdette <clburdette@yahoo.com>

---
+++ Hmwk/cis14astoreproject/getForm.js
@@ -0,0 +1,14 @@
+function getForm(url){
+        var $_GET = new Object;
+	var info=url.split("?");
+        if(info.length>1){
+            var nameValuePairs=info[1].split("&");
+            for(var i=0;i<nameValuePairs.length;i++){
+                    var map=nameValuePairs[i].split("=");
+                    var name=map[0];
+                    var value=map[1];
+                    $_GET[name]=value;
+            }
+        }
+	return $_GET;
+}
---
+++ Hmwk/cis14astoreproject/users.js
@@ -0,0 +1,77 @@
+users=[
+        {  title:"Beamrider",
+           year:"1983",
+           rarity:"rare",
+	   price:"5"	
+        },
+        {  title:"Boulder Dash",
+           year:"1982",
+           rarity:"rare",
+	   price:"9"
+        },
+        {  title:"BurgerTime",
+           year:"1982",
+           rarity:"common",
+	   price:"7"
+        },
+        {  title:"Carnival",
+           year:"1982",
+           rarity:"common",
+	   price:"3"
+        },
+        {  title:"Centipede",
+           year:"1983",
+           rarity:"common",
+	   price:"8"
+        },
+        {  title:"Decathlon",
+           year:"1983",
+           rarity:"very rare",
+	   price:"13"
+        },
+        {  title:"Defender",
+           year:"1983",
+           rarity:"rare",
+	   price:"5"
+        },
+        {  title:"Frenzy",
+           year:"1982",
+           rarity:"common",
+	   price:"3"
+        },
+        {  title:"Frogger",
+           year:"1984",
+           rarity:"common",
+	   price:"12"
+        },
+        {  title:"Gorf",
+           year:"1982",
+           rarity:"common",
+	   price:"4"
+        },
+        {  title:"Illusions",
+           year:"1984",
+           rarity:"very rare",
+	   price:"17"
+        },
+        {  title:"Popeye",
+           year:"1984",
+           rarity:"rare",
+	   price:"7"
+        },
+        {  title:"Smurf",
+           year:"1983",
+           rarity:"common",
+	   price:"3"
+        },
+	{  title:"Galaxian",
+           year:"1983",
+           rarity:"common",
+	   price:"7"
+        },
+	{  title:"Gyruss",
+           year:"1984",
+           rarity:"rare",
+	   price:"13"
+        }
+];
